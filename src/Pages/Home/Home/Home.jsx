51e4e5b92cee6aafe490f6bb163981b0b4283af1
rimu-7 <rrimu99@gmail.com>
2025-02-16 09:25:29 +0600

    home

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -10,51 +10,65 @@ function Home() {
   function handleNext() {
     if (step < 3) setStep(step + 1);
   }
-  return (
-    <div className="h-screen bg-[#212121]  text-center p-10 items-center flex justify-center  text-xl">
-      <div className=" w-[600px] bg-[#f7f7f7] rounded-lg px-5 py-5 my-[100px] mx-auto ">
-        <div className=" flex text-white font-bold justify-between">
-          <div
-            className={`h-10 aspect-square rounded-full flex items-center justify-center text-[18px] ${
-              step >= 1 ? "bg-blue-500" : "bg-[#e7e7e7] text-black"
-            }`}
-          >
-            1
-          </div>
-          <div
-            className={`h-10 aspect-square rounded-full flex items-center justify-center text-[18px] ${
-              step >= 2 ? "bg-blue-500" : "bg-[#e7e7e7] text-black"
-            }`}
-          >
-            2
-          </div>
-          <div
-            className={`h-10 aspect-square rounded-full flex items-center justify-center text-[18px] ${
-              step >= 3 ? "bg-blue-500" : "bg-[#e7e7e7] text-black"
-            }`}
-          >
-            3
-          </div>
-        </div>
-        <p className=" p-10">
-          Step{step}: {data[step - 1]}
-        </p>
 
-        <div className="flex justify-between text-white">
-          <button
-            className="bg-blue-500  rounded-md px-2 cursor-pointer hover:bg-blue-600 duration-300"
-            onClick={handlePrevious}
-          >
-            Previous
-          </button>
-          <button
-            className="bg-blue-500  rounded-md px-2 cursor-pointer hover:bg-blue-600 duration-300"
-            onClick={handleNext}
-            // onMouseEnter={() => alert("Test")}
-          >
-            Next
-          </button>
-        </div>
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="">
+      <div className="">
+        <button
+          className="absolute top-4 right-4 border-none bg-transparent cursor-pointer text-4xl text-inherit hover:text-blue-900"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          &times;
+        </button>
+        {isOpen && (
+          <div className="h-screen   text-center p-10 items-center flex justify-center  text-xl">
+            <div className=" w-[600px] bg-neutral-200 rounded-lg px-5 py-5 my-[100px] mx-auto ">
+              <div className=" flex text-white font-bold justify-between">
+                <div
+                  className={`h-10 aspect-square rounded-full flex items-center justify-center text-[18px] ${
+                    step >= 1 ? "bg-blue-500" : "bg-[#e7e7e7] text-black"
+                  }`}
+                >
+                  1
+                </div>
+                <div
+                  className={`h-10 aspect-square rounded-full flex items-center justify-center text-[18px] ${
+                    step >= 2 ? "bg-blue-500" : "bg-[#e7e7e7] text-black"
+                  }`}
+                >
+                  2
+                </div>
+                <div
+                  className={`h-10 aspect-square rounded-full flex items-center justify-center text-[18px] ${
+                    step >= 3 ? "bg-blue-500" : "bg-[#e7e7e7] text-black"
+                  }`}
+                >
+                  3
+                </div>
+              </div>
+              <p className=" p-10">
+                Step{step}: {data[step - 1]}
+              </p>
+
+              <div className="flex justify-between text-white">
+                <button
+                  className="bg-blue-500  rounded-md px-2 cursor-pointer hover:bg-blue-600 duration-300"
+                  onClick={handlePrevious}
+                >
+                  Previous
+                </button>
+                <button
+                  className="bg-blue-500  rounded-md px-2 cursor-pointer hover:bg-blue-600 duration-300"
+                  onClick={handleNext}
+                  // onMouseEnter={() => alert("Test")}
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
